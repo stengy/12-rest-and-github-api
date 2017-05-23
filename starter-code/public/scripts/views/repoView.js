ac640786d528e16b1906c8a51cf742b305d357bf
Scott <sdmccoy@gmail.com>
2017-05-23 15:11:18 -0700

    created a gitignore, added a githubtoken file and stored the token in there. Added the scripts for all js files on the index page

--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -14,6 +14,11 @@ var app = app || {};
 
   // TODO: Remember that new Handlebars template? Let's compile it!
   // Save the result in this `render` variable.
+  repoView.prototype.toHtml = function() {
+    let render = Handlebars.compile($('#repo-template').text());
+    return render();
+  }
+
 
   repoView.index = function() {
     ui();
--- starter-code/server.js
+++ starter-code/server.js
@@ -6,8 +6,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-// const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = ''; // TODO: Don't forget to set your own conString
+const conString = 'postgres://postgres:dingodb@localhost:5432/kilovolt';
+// const conString = ''; // DONE TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
